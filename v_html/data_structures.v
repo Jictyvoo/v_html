f33269b30c8d26252afb0a5fb7f699c8e962728e
Jictyvoo <jictyvoo.ecomp@gmail.com>
2019-12-16 12:09:06 -0300

    Mother of Segfaults, please help me

--- v_html/data_structures.v
+++ v_html/data_structures.v
@@ -40,32 +40,42 @@ fn (stack mut Stack) push(item int) {
 	stack.size++
 }
 
-struct BTreeNode {
-	element Tag
-	mut:
-		childrens []BTreeNode
-}
-
 struct BTree {
 	mut:
-		all_nodes []BTreeNode
+		all_tags []Tag
 		node_pointer int = 0
+		childrens [][]int
 }
 
 fn (btree mut BTree) add_children(tag Tag) int {
-	btree.all_nodes << BTreeNode{element: tag}
-	if btree.all_nodes.len > 1 {
-		btree.all_nodes[btree.node_pointer].childrens << btree.all_nodes[btree.all_nodes.len - 1] //segfault
+	println("OK_1")
+	btree.all_tags << tag
+	println("OK_2")
+	if btree.all_tags.len > 1 {
+		println("OK_3")
+		for btree.childrens.len <= btree.node_pointer {		
+			println("OK_4")
+			btree.childrens << []int
+			println("OK_5 >> " + btree.childrens.len.str())
+		}
+		println("OK_6")
+		mut temp_array := btree.childrens[btree.node_pointer]
+		temp_array << btree.all_tags.len - 1
+		println("OK_7")
 	}
-	return btree.all_nodes.len - 1
+	return btree.all_tags.len - 1
 }
 
-fn (btree BTree) get_children() []BTreeNode {
-	return btree.all_nodes[btree.node_pointer].childrens
+fn (btree BTree) get_children() []int {
+	return btree.childrens[btree.node_pointer]
+}
+
+fn (btree BTree) get_stored() Tag {
+	return btree.all_tags[btree.node_pointer]
 }
 
 fn (btree mut BTree) move_pointer(to int) {
-	if to < btree.all_nodes.len {
+	if to < btree.all_tags.len {
 		btree.node_pointer = to
 	}
 }
